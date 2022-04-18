24d58c75852be7f0821560028c571d470dc03d63
Gray <1752882923@qq.com>
2022-04-18 09:31:49 +0800

    first commit

---
+++ src/main/java/ltd/newbee/mall/interceptor/NewBeeMallLoginInterceptor.java
@@ -0,0 +1,47 @@
+/**
+ * 严肃声明：
+ * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
+ * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
+ * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
+ * Copyright (c) 2021-2022 梁家炜 all rights reserved.
+ * 版权所有，侵权必究！
+ */
+package ltd.newbee.mall.interceptor;
+
+import ltd.newbee.mall.common.Constants;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 大学生闲置物品交易平台系统身份验证拦截器
+ *
+ * @author 梁家炜
+ * @email 1752882923@qq.com
+ * @link https://192.168.153.1:28089
+ */
+@Component
+public class NewBeeMallLoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+        if (null == request.getSession().getAttribute(Constants.MALL_USER_SESSION_KEY)) {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
